1832aa1f12514179c3db15ee3e5315b78f484125
Ephraim Quarcoo <ephriamniiarkaaquarco649002@gmail.com>
2023-03-20 08:33:48 -0700

    Update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Program prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int c = 0; c <= 9; c++)
+	{
+		if (c != 'e' || c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+		return (0);
+}
