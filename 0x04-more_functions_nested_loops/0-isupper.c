b3c018663f86c8394ffb5d3654fd67120ee68d28
Kojo <ephraimquarcoo70@gmail.com>
2023-03-23 22:55:32 +0000

    create funtion that checks for upper case characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+
+int _isupper(int c);
+
+	/*Assign c to int ch */
+	int ch = 'c';
+	
+	switch (ch)
+	{
+		case (ch > 64 && ch < 91):
+			return (1);
+			break;
+		default:
+			return (0);
+			break;
+	}
