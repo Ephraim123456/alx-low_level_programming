e933c84bdf1092d37ecd360ae0a8d346d98991ec
Kojo <ephraimquarcoo70@gmail.com>
2023-03-23 23:41:15 +0000

    Update to betty coding style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
-
+/**
+ * _isupper - check for upper case character
+ * @c: character
+ * Return: 1 if true and 0 if false
+ */
 int _isupper(int c)
 {
 
 	/*Assign c to int ch */
 	int ch = c;
-	
-	if ( ch > 64 && ch <91)
+
+	if (ch > 64 && ch < 91)
 	{
-	
 		return (1);
 	}
 	else
