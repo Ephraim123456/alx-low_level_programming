7206bd1aa4b6fbcefde8deeaa3c4de7870610137
Kojo <ephraimquarcoo70@gmail.com>
2023-03-24 00:55:53 +0000

    update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define main_h
 int _isupper(int c);
 int _isdigit(int c);
-int _putchar(char c);
 int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
