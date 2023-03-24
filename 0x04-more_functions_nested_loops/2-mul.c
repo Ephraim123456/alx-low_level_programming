5eec19b44761cfec3781b328fd425a73d8717289
Kojo <ephraimquarcoo70@gmail.com>
2023-03-24 00:28:42 +0000

    created function mul;multiples products of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - multiples two numbers
+ * @a:firt integer value
+ * @b: second integer value
+ * Return: mult (product of two number)
+ *
+ */
+
+int mul(int a, int b)
+{
+	int num1 = a;
+	int num2 = b;
+	int prod = a * b;
+
+	return (prod);
+}
