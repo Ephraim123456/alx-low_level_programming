b701eb2ce5a3e5b213914dd385ff420d5af435b4
Kojo <ephraimquarcoo70@gmail.com>
2023-03-24 00:04:19 +0000

    Main updated with function prototype, create funtion to check for digits from 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+/**
+ * _isdigit - checks for digits 0 through 9
+ * @c: accepts characters
+ * Return: 1 if c is a digit and 0 if false
+ */
+
+int _isdigit(int c)
+{
+	int ch = c;
+
+	if (ch > 46 && ch < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
