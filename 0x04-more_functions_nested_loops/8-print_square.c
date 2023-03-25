a36f28d721de3952be2001603a77ea2a192fb0c4
Kojo <ephraimquarcoo70@gmail.com>
2023-03-25 03:48:01 +0000

    Create print_square(int size)

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: accepts interger value
+ */
+
+void print_square(int size)
+{
+
+	int i, k;
+
+	while (i < size)
+	{
+		for (k = 0; k < size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
