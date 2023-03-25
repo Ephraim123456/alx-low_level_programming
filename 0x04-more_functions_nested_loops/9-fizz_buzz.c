8741f970df0941ec3e35c72404c86e64f8792b58
Kojo <ephraimquarcoo70@gmail.com>
2023-03-25 05:14:10 +0000

    Update code with nested if statement

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,39 +6,24 @@
 
 int main(void)
 {
-	int i = 1;
-
-	while (i <= 100)
+	for (int i = 1; i <= 100; i++)
 	{
-
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			if (i == 100)
-			{
-				printf("Buzz");
-			}
-			else
-			{
-				printf("Buzz ");
-			}
-		}
-		else if (i % 5 == 0 && i % 3 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else
+			printf("Buzz ");
+		} else
 		{
 			printf("%d ", i);
 		}
-			i++;
-
 	}
-	printf("\n");
-
+			printf("\n");
 	return (0);
-
 }
+
