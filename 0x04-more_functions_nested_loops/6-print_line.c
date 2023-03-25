92d2496e3a3503c04ac3ed43d055f3c04167c2ac
Kojo <ephraimquarcoo70@gmail.com>
2023-03-25 02:46:35 +0000

    create function 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,7 @@ void print_line(int n)
 
 	for (i = 0; i < n;  i++)
 	{
-		if (n == 0 || n < 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-			_putchar('\n');
-		}
+		_putchar('-');
 	}
-
+	_putchar('\n');
 }
