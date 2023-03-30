1fb4b648d4ccabbc86f6387ddc276f308faaade2
root <root@DESKTOP-8O20H77.localdomain>
2023-03-30 11:00:29 +0000

    Function to swap two pointer variable

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - Function swaps variable values
+ * @a: first pointer variable
+ * @b: second pointer variable
+ */
+void swap_int(int *a, int *b)
+{
+	int temp, *a, *b;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
