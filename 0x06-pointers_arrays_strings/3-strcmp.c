64c2995c771a7c07f05135c3aa1df5ffd161dba0
root <root@DESKTOP-8O20H77.localdomain>
2023-03-31 02:15:29 +0000

    Create functions to concatenate and copy strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string pointer
+ * @s2: second string pointer
+ * Return: strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
