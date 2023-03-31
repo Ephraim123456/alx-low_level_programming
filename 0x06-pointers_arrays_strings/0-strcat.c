f687b436591534248156cb4025ce5508e93ad2c3
root <root@DESKTOP-8O20H77.localdomain>
2023-03-31 01:31:07 +0000

    more functions project

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+
+#include <string.h>
+/**
+ * *_strcat - appends the src string to dest string
+ * @dest: destination string
+ * @src: source string
+ * Return: returns a pointer to the resulting string destination
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	return(strcat( dest, src));
+}
