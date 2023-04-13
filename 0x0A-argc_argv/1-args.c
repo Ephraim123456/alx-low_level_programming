ffdd31cab11cee16a3bf40c0ecfc051e89000593
root <root@DESKTOP-8O20H77.localdomain>
2023-04-13 11:49:01 +0000

    Printing number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
