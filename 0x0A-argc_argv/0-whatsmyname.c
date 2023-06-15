bef79c09dadb9642f8f1e02b869133aa29b23536
Ephraim <ephriamniiarkaaquarco649002@gmail.com>
2023-06-15 00:28:27 +0000

    Argc_Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (sucess)
+ *
+ */
+int main(int argc, *char argv[])
+{
+	printf("%s\n", argv[0]);
+
+return (0);
+}
