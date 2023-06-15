182ece806380e21ee3a6aed08ccd15f222b46386
Ephraim <ephriamniiarkaaquarco649002@gmail.com>
2023-06-15 03:27:52 +0000

    Argc_Argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,28 +8,27 @@
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
-{	int i, sum;
+{	int i, sum = 0;
 
-	if (argc > 1 && argc !isdigit)
+	if (argc > 1)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	if (argc == 1)
-	{
-		printf("0\n");
+		for (i = 1; i < argc; i++)
+		{
+			int j;
 
-	}
-	else if (argc > 1)
-	{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argc[i]);
-	}
+			sum += atoi(argv[i];
 
+					}
 		printf("%d\n", sum);
 	}
-
 	return (0);
 }
