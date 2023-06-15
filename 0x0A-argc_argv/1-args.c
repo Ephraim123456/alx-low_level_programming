7d4ccbd858822ed648393272a8615acc19d4539b
Ephraim <ephriamniiarkaaquarco649002@gmail.com>
2023-06-15 01:28:45 +0000

    Argc_Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument counts
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, *char argv[]
+{
+	(void)argv:	
+	printf("%d", argc);
+	return (0);
+}
